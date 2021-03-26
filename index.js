04d69f43ff7b13b557f4cb0467dda4f8a3a03798
Alex Howez <aech-12@hotmail.com>
2021-03-25 21:50:38 -0300

    start server

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ console.log(`Server running on port ${PORT}`)
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
+
+app.get('/videos', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
