1a77023096a007b7bf7d4b7a7d3ef5f6427eff4a
Alex Howez <aech-12@hotmail.com>
2021-03-25 21:32:10 -0300

    start server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+app.listen(PORT)
+
+console.log(`Server running on port ${PORT}`)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
